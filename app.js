7a4889bad7f5ee0bb99f80228a82f66fdb679793
Jackson Ozanne <s537079@nwmissouri.edu>
2025-03-27 23:50:26 -0500

    endpoint mydata

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
@@ -15,13 +14,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use('/mydata', mydataRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', mydataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
